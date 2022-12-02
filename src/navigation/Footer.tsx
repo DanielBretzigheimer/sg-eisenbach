226618480d50bc5129131fa11606b058f7650237
Daniel Bretzigheimer <dbretzigheimer95@gmail.com>
2022-12-03 00:46:56 +0100

    Footer updated

--- src/navigation/Footer.tsx
+++ src/navigation/Footer.tsx
@@ -9,7 +9,7 @@ export function Footer() {
   const navigate = useNavigate()
 
   return (
-    <Box sx={{ display: "flex", gap: 2 }}>
+    <Box gap={2} sx={{ display: "flex" }} mb={2}>
       <Stack flexGrow={1}>
         <Typography variant="caption">© 2020 SG Eisenbach 1958 e.V.</Typography>
       </Stack>
